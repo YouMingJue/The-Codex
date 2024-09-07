289a5757aa0bb3ae23deb3bee1f2803d1af48682
jiahao <robinbschool@gmail.com>
2024-09-07 14:55:37 -0400

    Tile generation set to generate in the editor instead of runtime

--- Assets/Scripts/Tile and Grid/TileManager.cs
+++ Assets/Scripts/Tile and Grid/TileManager.cs
@@ -1,37 +1,59 @@
 using UnityEngine;
+using UnityEditor; // Required to use Editor functionality
 
+[ExecuteInEditMode] // This attribute allows the script to run in the editor
 public class TileManager : MonoBehaviour
 {
-    public GameObject tilePrefab; // 地块预制体
-    public int width = 10; // 网格宽度
-    public int height = 10; // 网格高度
-    public Vector2Int playerStartPosition; // 玩家生成位置
+    public GameObject tilePrefab;
+    public int width = 10;
+    public int height = 10;
+    public Vector2Int playerStartPosition;
 
-    private Tile[,] tiles; // 存储网格中的地块
+    private Tile[,] tiles;
 
     void Start()
     {
-        GenerateGrid();
+        // Optionally you can remove runtime grid generation
     }
 
-    public Vector3 GetPlayerStartPosition()
+    [ContextMenu("Generate Grid")] // Adds this function to the right-click menu in the inspector
+    public void GenerateGrid()
     {
-        // 确保玩家的起始位置在2D平面上
-        return new Vector3(playerStartPosition.x, playerStartPosition.y, 0);
-    }
+        // Clear any existing tiles to avoid duplicates
+        ClearGrid();
 
-    void GenerateGrid()
-    {
         tiles = new Tile[width, height];
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                // 在2D游戏中，通常Z轴坐标为0
                 Vector3 position = new Vector3(x, y, 0);
-                GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, transform);
+                GameObject tile = PrefabUtility.InstantiatePrefab(tilePrefab, transform) as GameObject;
+                tile.transform.position = position;
                 tiles[x, y] = tile.GetComponent<Tile>();
+
+                // Mark the tile as part of the scene for saving
+                Undo.RegisterCreatedObjectUndo(tile, "Create Tile");
             }
         }
+
+        // Mark the scene as dirty to ensure changes are saved
+        EditorUtility.SetDirty(gameObject);
+    }
+
+    // Clear the grid in the editor
+    [ContextMenu("Clear Grid")]
+    public void ClearGrid()
+    {
+        // Destroy all existing tiles
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Undo.DestroyObjectImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public Vector3 GetPlayerStartPosition()
+    {
+        return new Vector3(playerStartPosition.x, playerStartPosition.y, 0);
     }
 }
